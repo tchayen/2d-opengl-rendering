1268e974216bd4c62404bea55a1eaa46ad594a0e
Tomasz Czajęcki <tomek.czajecki@gmail.com>
2016-07-29 03:01:37 +0200

    Added ability to update positions of objects

--- main.cpp
+++ main.cpp
@@ -2,32 +2,39 @@
 #include <cstdlib>
 #include <ctime>
 
+// you will need your own OpenGL bindings, GLFW (or other lib capable of creating
+// window and OpenGL context) and SOIL (or, again, other lib loading images in
+// format of your choice)
 #include "glew-1.9.0\glew.h"
 #include "glfw-3.2\glfw3.h"
 #include "soil-1.16\SOIL.h"
 
-typedef signed char        int8;
+// not necessary but makes my life much easier when I can just see the number of
+// bits and I dont have to write 'unsigned' as a separate word
 typedef short              int16;
 typedef int                int32;
-typedef long long          int64;
-typedef unsigned char      uint8;
 typedef unsigned short     uint16;
 typedef unsigned int       uint32;
-typedef unsigned long long uint64;
 
 void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode);
 GLuint getShaderProgramId(const char *vertexFile, const char *fragmentFile);
 GLuint compileShader(const GLchar *source, GLuint shaderType);
 
-GLFWwindow* window = 0;
+GLFWwindow* window;
 
 const uint32
     WINDOW_WIDTH = 1440,
     WINDOW_HEIGHT = 810,
-    OBJECT_COUNT = 1000;
+    // object count is static number in this example, but it can be made a bit more
+    // flexible (however remember that you will never have much freedom with constant
+    // block of memory which is required by targeted performance)
+    OBJECT_COUNT = 100;
 
-GLuint shaderProgramId, vao, vbo, ubo;
+GLuint shaderProgramId, vao, vbo, ubo, textureId;
 
+// in order to avoid sending information to shaders screen resolution is hard coded
+// (precisely, half of it in each dimension), but it should be unnoticable in when
+// extracted to shader uniform
 const char *vertexShader =
     "#version 330\n"
     "layout (location = 0) in vec2 vert;\n"
@@ -50,31 +57,88 @@ const char *fragmentShader =
     "}\n";
 
 struct Texture { uint16 width, height; float u1, v1, u2, v2; };
-struct Object { uint16 x, y; Texture texture; };
+struct Object  { int16 x, y; Texture texture; };
 
 Texture
-    watermelon = { 64, 64, 0.0f, 0.0f, 0.5f, 0.5f },
-    pineapple  = { 64, 64, 0.5f, 0.0f, 1.0f, 0.5f },
-    orange     = { 32, 32, 0.0f, 0.5f, 0.25f, 0.75f },
-    grapes     = { 32, 32, 0.25f, 0.5f, 0.5f, 0.75f },
-    pear       = { 32, 32, 0.0f, 0.75f, 0.25f, 1.0f },
-    banana     = { 32, 32, 0.25f, 0.75f, 0.5f, 1.0f },
-    strawberry = { 16, 16, 0.5f, 0.5f, 0.625f, 0.625f },
-    raspberry  = { 16, 16, 0.625f, 0.5f, 0.75f, 0.625f },
-    cherries   = { 16, 16, 0.5f, 0.625f, 0.625f, 0.75f };
+    watermelon = { 64, 64, 0.0f,   0.0f,   0.5f,   0.5f   },
+    pineapple  = { 64, 64, 0.5f,   0.0f,   1.0f,   0.5f   },
+    orange     = { 32, 32, 0.0f,   0.5f,   0.25f,  0.75f  },
+    grapes     = { 32, 32, 0.25f,  0.5f,   0.5f,   0.75f  },
+    pear       = { 32, 32, 0.0f,   0.75f,  0.25f,  1.0f   },
+    banana     = { 32, 32, 0.25f,  0.75f,  0.5f,   1.0f   },
+    strawberry = { 16, 16, 0.5f,   0.5f,   0.625f, 0.625f },
+    raspberry  = { 16, 16, 0.625f, 0.5f,   0.75f,  0.625f },
+    cherries   = { 16, 16, 0.5f,   0.625f, 0.625f, 0.75f  };
 
-Texture textures[9] = { watermelon, pineapple, orange, grapes, pear, banana, strawberry, cherries, raspberry };
+Texture textures[9] =
+{
+    watermelon,
+    pineapple,
+    orange,
+    grapes,
+    pear,
+    banana,
+    strawberry,
+    cherries,
+    raspberry
+};
 
 static Object objects[OBJECT_COUNT];
 
-static uint16 vertices[OBJECT_COUNT * 12];
+static int16 vertices[OBJECT_COUNT * 12];
 static float uvs[OBJECT_COUNT * 12];
+
+void updateObject(int i)
+{
+    // top right
+    vertices[i * 12] = objects[i].x + objects[i].texture.width;
+    vertices[i * 12 + 1] = objects[i].y;
+
+    // bottom right
+    vertices[i * 12 + 2] = objects[i].x + objects[i].texture.width;
+    vertices[i * 12 + 3] = objects[i].y + objects[i].texture.height;
+
+    // top left
+    vertices[i * 12 + 4] = objects[i].x;
+    vertices[i * 12 + 5] = objects[i].y;
+
+    // bottom right
+    vertices[i * 12 + 6] = objects[i].x + objects[i].texture.width;
+    vertices[i * 12 + 7] = objects[i].y + objects[i].texture.height;
+
+    // bottom left
+    vertices[i * 12 + 8] = objects[i].x;
+    vertices[i * 12 + 9] = objects[i].y + objects[i].texture.height;
+
+    // top left
+    vertices[i * 12 + 10] = objects[i].x;
+    vertices[i * 12 + 11] = objects[i].y;
+}
+
+void update()
+{
+    // dancing fruits =)
+    for (uint32 i = 0; i < OBJECT_COUNT; i++)
+    {
+        objects[i].x += rand() % 5 - 2;
+        objects[i].y += rand() % 5 - 2;
+
+        updateObject(i);
+    }
+
+    // if you had to unbind vbo for whatever reason, bind it again now
+    // glBindBuffer(GL_ARRAY_BUFFER, vbo);
+    glBufferSubData(GL_ARRAY_BUFFER, 0, sizeof(vertices), vertices);
+    
+}
 
 void render()
 {
     glClearColor(0.2f, 0.25f, 0.3f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT);
 
+    // if you had to unbind vao for whatever reason, bind it again now
+    // glBindVertexArray(vao);
     glDrawArrays(GL_TRIANGLES, 0, OBJECT_COUNT * 6);
 
     glfwSwapBuffers(window);
@@ -82,6 +146,7 @@ void render()
 
 int32 main()
 {
+    // not perfect but good enough randomness
     srand(time(0));
 
     // initialize GLFW
@@ -118,92 +183,94 @@ int32 main()
     shaderProgramId = getShaderProgramId(vertexShader, fragmentShader);
 
     // texture
-    GLuint texture = 0;
-
-    glGenTextures(1, &texture);
-    glBindTexture(GL_TEXTURE_2D, texture);
+    glGenTextures(1, &textureId);
+    glBindTexture(GL_TEXTURE_2D, textureId);
 
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
 
-    int32 imageWidth, imageHeight;
-    unsigned char* image = SOIL_load_image("assets/texture.png", &imageWidth, &imageHeight, 0, SOIL_LOAD_RGBA);
-    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, imageWidth, imageHeight, 0, GL_RGBA, GL_UNSIGNED_BYTE, image);
-
-    if (image == 0)
+    // load image using SOIL and store it on GPU
     {
-        printf("Failed to load texture image.\n");
-        exit(1);
-    }
-    else
-        SOIL_free_image_data(image);
+        int32 width, height;
+        unsigned char* image = SOIL_load_image("texture.png", &width, &height, 0, SOIL_LOAD_RGBA);
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, image);
 
-    // prepare objects, transfer them to buffers
-    for (uint16 i = 0; i < OBJECT_COUNT; i++)
+        if (image == 0)
+        {
+            printf("Failed to load texture image.\n");
+            exit(1);
+        }
+        else
+        {
+            SOIL_free_image_data(image);
+        }
+    }
+
+    for (uint32 i = 0; i < OBJECT_COUNT; i++)
     {
         Texture t = textures[rand() % 9];
         objects[i] =
         {
-            rand() % (WINDOW_WIDTH - t.width),
-            rand() % (WINDOW_HEIGHT - t.height),
+            (int16)(rand() % (WINDOW_WIDTH - t.width)),
+            (int16)(rand() % (WINDOW_HEIGHT - t.height)),
             t
         };
-    }
 
-    for (uint32 i = 0; i < OBJECT_COUNT; i++)
-    {
-        // top right
-        vertices[i * 12] = objects[i].x + objects[i].texture.width;
-        vertices[i * 12 + 1] = objects[i].y;
+        // vertices
+        {
+            // top right
+            vertices[i * 12] = objects[i].x + objects[i].texture.width;
+            vertices[i * 12 + 1] = objects[i].y;
 
-        // bottom right
-        vertices[i * 12 + 2] = objects[i].x + objects[i].texture.width;
-        vertices[i * 12 + 3] = objects[i].y + objects[i].texture.height;
+            // bottom right
+            vertices[i * 12 + 2] = objects[i].x + objects[i].texture.width;
+            vertices[i * 12 + 3] = objects[i].y + objects[i].texture.height;
 
-        // top left
-        vertices[i * 12 + 4] = objects[i].x;
-        vertices[i * 12 + 5] = objects[i].y;
+            // top left
+            vertices[i * 12 + 4] = objects[i].x;
+            vertices[i * 12 + 5] = objects[i].y;
 
-        // bottom right
-        vertices[i * 12 + 6] = objects[i].x + objects[i].texture.width;
-        vertices[i * 12 + 7] = objects[i].y + objects[i].texture.height;
+            // bottom right
+            vertices[i * 12 + 6] = objects[i].x + objects[i].texture.width;
+            vertices[i * 12 + 7] = objects[i].y + objects[i].texture.height;
 
-        // bottom left
-        vertices[i * 12 + 8] = objects[i].x;
-        vertices[i * 12 + 9] = objects[i].y + objects[i].texture.height;
+            // bottom left
+            vertices[i * 12 + 8] = objects[i].x;
+            vertices[i * 12 + 9] = objects[i].y + objects[i].texture.height;
 
-        // top left
-        vertices[i * 12 + 10] = objects[i].x;
-        vertices[i * 12 + 11] = objects[i].y;
-    }
+            // top left
+            vertices[i * 12 + 10] = objects[i].x;
+            vertices[i * 12 + 11] = objects[i].y;
+        }
 
-    for (uint32 i = 0; i < OBJECT_COUNT; i++)
-    {
-        // top right
-        uvs[i * 12] = objects[i].texture.u2;
-        uvs[i * 12 + 1] = objects[i].texture.v2;
+        // uvs
+        {
+            // top right
+            uvs[i * 12] = objects[i].texture.u2;
+            uvs[i * 12 + 1] = objects[i].texture.v2;
 
-        // bottom right
-        uvs[i * 12 + 2] = objects[i].texture.u2;
-        uvs[i * 12 + 3] = objects[i].texture.v1;
+            // bottom right
+            uvs[i * 12 + 2] = objects[i].texture.u2;
+            uvs[i * 12 + 3] = objects[i].texture.v1;
 
-        // top left
-        uvs[i * 12 + 4] = objects[i].texture.u1;
-        uvs[i * 12 + 5] = objects[i].texture.v2;
+            // top left
+            uvs[i * 12 + 4] = objects[i].texture.u1;
+            uvs[i * 12 + 5] = objects[i].texture.v2;
 
-        // bottom right
-        uvs[i * 12 + 6] = objects[i].texture.u2;
-        uvs[i * 12 + 7] = objects[i].texture.v1;
+            // bottom right
+            uvs[i * 12 + 6] = objects[i].texture.u2;
+            uvs[i * 12 + 7] = objects[i].texture.v1;
 
-        // bottom left
-        uvs[i * 12 + 8] = objects[i].texture.u1;
-        uvs[i * 12 + 9] = objects[i].texture.v1;
+            // bottom left
+            uvs[i * 12 + 8] = objects[i].texture.u1;
+            uvs[i * 12 + 9] = objects[i].texture.v1;
 
-        // top left
-        uvs[i * 12 + 10] = objects[i].texture.u1;
-        uvs[i * 12 + 11] = objects[i].texture.v2;
+            // top left
+            uvs[i * 12 + 10] = objects[i].texture.u1;
+            uvs[i * 12 + 11] = objects[i].texture.v2;
+        }
     }
 
     // initialize OpenGL buffers
@@ -213,8 +280,8 @@ int32 main()
     glBindVertexArray(vao);
 
     glBindBuffer(GL_ARRAY_BUFFER, vbo);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-    glVertexAttribPointer(0, 2, GL_UNSIGNED_SHORT, GL_FALSE, 2 * sizeof(uint16), 0);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_DYNAMIC_DRAW);
+    glVertexAttribPointer(0, 2, GL_SHORT, GL_FALSE, 2 * sizeof(int16), 0);
 
     glBindBuffer(GL_ARRAY_BUFFER, ubo);
     glBufferData(GL_ARRAY_BUFFER, sizeof(uvs), uvs, GL_STATIC_DRAW);
@@ -226,10 +293,10 @@ int32 main()
     glBindBuffer(GL_ARRAY_BUFFER, 0);
     glBindVertexArray(0);
 
-    // those usually go to render(), but because program uses one shader, vao and texture it is enough to set them once
+    // those usually go to render(), but because this example uses one shader, vao and texture it is enough to set them once
     glUseProgram(shaderProgramId);
     glActiveTexture(GL_TEXTURE0);
-    glBindTexture(GL_TEXTURE_2D, texture);
+    glBindTexture(GL_TEXTURE_2D, textureId);
     glBindVertexArray(vao);
 
     // main loop
@@ -246,6 +313,8 @@ int32 main()
         double frameTime = newTime - currentTime;
         currentTime = newTime;
 
+        // print frame time
+        // if you want to get rid of that remember to also delete lastPrinted variable above
         if (currentTime - lastPrinted > 1.0)
         {
             printf("%fms\n", frameTime * 1000.0);
@@ -257,6 +326,7 @@ int32 main()
         while (accumulator >= dt)
         {
             glfwPollEvents();
+            update();
             accumulator -= dt;
             t += dt;
         }
@@ -264,10 +334,11 @@ int32 main()
         render();
     }
 
+    // cleanup
     glDeleteVertexArrays(1, &vao);
     glDeleteBuffers(1, &vbo);
     glDeleteBuffers(1, &ubo);
-    glDeleteTextures(1, &texture);
+    glDeleteTextures(1, &textureId);
     glDeleteProgram(shaderProgramId);
 
     glfwTerminate();
